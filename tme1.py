6775acca6b4562b75615e474d1dbbb3a7b9ab563
keyber <ad.spif@gmail.com>
2019-02-07 18:02:33 +0100

    todo regex

--- tme1.py
+++ tme1.py
@@ -45,4 +45,96 @@ def create_tf_idf(list_docs):
     
     return {i: {w: tf_idf(i, w) for w in index[i].keys()} for i in range(len(list_docs))}
 
-print(create_tf_idf(docs))
+#print(create_tf_idf(docs))
+
+
+
+##########   2) PARSING FICHIER        #######
+balises = {'I', 'T', 'B', 'A', 'K', 'W', 'X'}
+balises = {'.'+c for c in balises}
+bal_i = '.I'
+bal_t = '.T'
+
+def buildDocCollectionSimple(file_path):
+    res = []
+    with open(file_path) as f:
+        s = f.readline()
+        
+        while s:
+            #se place à la première balise I
+            while s[:2] != bal_i and s:
+                s = f.readline()
+            
+            #pas de balise I, fin du doc
+            if not s:
+                break
+            
+            #l'indice du document est sur la même ligne que la balise I
+            idoc = s.split()[1]
+            
+            lines = []
+            
+            s = f.readline()
+            #cherche balise T (ou I, auquel cas il n'y a pas de T)
+            while s[:2] != bal_t and s[:2] != bal_i and s:
+                s = f.readline()
+                
+            if s[:2] == bal_t:
+                s = f.readline()
+                #copie tout jusqu'à rencontrer n'importe quelle balise
+                while s[:2] not in balises and s:
+                    lines.append(s)
+                    s = f.readline()
+            
+            res.append((idoc, lines))
+    return res
+
+
+#print(buildDocCollectionSimple("data/cacmShort.txt"))
+
+
+import re
+def buildDocumentCollectionRegex(file_path):
+    res = []
+    with open(file_path) as f:
+        texte = f.read().replace("\n", " ").split(bal_i)
+    for doc in texte[1:2]:
+        iDoc = re.search("\.[ITBAKWX]", doc)
+        req = re.findall("\.T .* \.[ITBAKWX]", doc)
+        print(req)
+        T = ""
+        if len(req):
+            T = req[0][3:-3]
+        res.append((iDoc, T))
+    return res
+
+buildDocumentCollectionRegex("data/cacmShort.txt")
+
+
+
+#normaliser sur index qui contient nb occ
+#inverse ne somme pas à un
+
+
+
+class Document:
+    def __init__(self, identifiant, texte):
+        self.I = identifiant
+        self.T = texte
+
+class Parser:
+    def __init__(self, docs):
+        self.docs = docs
+        
+class IndexerSimple:
+    def indexation(self):
+        pass
+    def getTfsForDoc(self):
+        pass
+    def getTfIdfsForDoc(self):
+        pass
+    def getTfsForStem(self):
+        pass
+    def getTfIDFsForStem(self):
+        pass
+    
