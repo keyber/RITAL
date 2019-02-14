c51f0512f2c45d8f94fb5319de5e220ac880a4db
rootNico <nicopsp@live.fr>
2019-02-14 15:22:54 +0100

    exo1 tme2

--- indexerSimple.py
+++ indexerSimple.py
@@ -3,9 +3,6 @@ import collections
 import numpy as np
 import textRepresenter
 
-#stopwords
-mots_vides = {'the', 'a', 'an', 'on', 'behind', 'under', 'there', 'in', 'on'}
-
 
 def counter(phrase):
     porter_stemer = textRepresenter.PorterStemmer()
@@ -18,51 +15,51 @@ def counter(phrase):
 class IndexerSimple:
     def __init__(self, docs):
         self.docs = docs
-    
-    
+
+
     def create_indexes(self):
         """ return (ind, ind_inv) (ind_norma, ind_inv_norma)
         ind : {iDoc: {w: occ}}
         inv : {w: {iDoc: occ}}
         """
-        
+
         all_words = set()
         ind = {}
         ind_n = {}
         for d in self.docs:
             count = counter(d.T)
-            
+
             all_words = all_words.union(set(count.keys()))
-            
+
             ind[d.I] = count
-            
+
             f = 1 / sum(count.values())
             ind_n[d.I] = {key:val*f for (key,val) in count.items()}
-        
+
         inv = {w: {d.I: ind[d.I][w] for d in self.docs if w in ind[d.I]} for w in all_words}
         inv_n = {w: {d.I: ind_n[d.I][w] for d in self.docs if w in ind_n[d.I]} for w in all_words}
 
         return (ind, inv), (ind_n, inv_n)
-    
-    
+
+
     def getTfsForDoc(self, ind, doc):
         print("à vérifier")
         return ind[doc]
-    
+
     def getTfIDFsForDoc(self, ind, inv, doc):
         #pas de "inv" en paramètre normalement ?
         print("à vérifier")
         return {np.log((1 + len(ind)) / (1 + len(inv[w]))) for w in ind[doc].keys()}
-    
+
     def getTfsForStem(self, inv, stem):
         print("à vérifier")
         return inv[stem]
-    
+
     def getTfIDFsForStem(self, inv, stem):
         print("à vérifier")
         idf = len(inv[stem])
         return {d:tf*idf for (d, tf) in inv[stem].items()}
-    
+
     def getStrDoc(self, doc):
         print("à vérifier")
         return doc.T
@@ -72,19 +69,17 @@ class IndexerSimple:
         """{iDoc: {w: tf-idf}}"""
         N = len(self.docs)
         (index, index_inverse), _ = self.create_indexes()
-    
+
         def tf(i, w):
             return index[i][w]
-    
+
         def df(w):
             return len(index_inverse[w])
-    
+
         def idf(w):
             return np.log((1 + N) / (1 + df(w)))
-    
+
         def tf_idf(i, w):
             return tf(i, w) * idf(w)
-    
+
         return {d.I: {w: tf_idf(d.I, w) for w in index[d.I].keys()} for d in self.docs}
-    
-        
--- test2.py
+++ test2.py
@@ -4,18 +4,20 @@ import tme2
 
 
 def test1():
-    docs = ["the new home has been saled on top forecasts",
+    docs = ["the new home has home been saled on top forecasts",
             "the home sales rise in july",
-            "there is an increase in home sales in july",
+            "there is an increase in sales in july",
             "july encounter a new home sales rise"]
-    
+
     parsed = parser.loadCollection(docs)
 
     indexer = indexerSimple.IndexerSimple(parsed.docs)
 
     #contenu des indexes
     (ind, inv), (ind_n, inv_n) = indexer.create_indexes()
-    
-    print(tme2.score(ind, inv, "home sales top"))
-    
+
+    #print(tme2.score("home sales top",inv))
+    print(tme2.score_vectoriel("home sales top",inv))
+
+
 test1()
