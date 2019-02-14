4f287921b766161916a19c3b2c1c9c27cfb70069
rootNico <nicopsp@live.fr>
2019-02-14 17:53:16 +0100

    dernier commit TME2

--- test2.py
+++ test2.py
@@ -1,7 +1,8 @@
 import indexerSimple
 import parser
 import tme2
-
+import weighter
+import vectoriel
 
 def test1():
     docs = ["the new home has home been saled on top forecasts",
@@ -15,4 +16,17 @@ def test1():
 
     #print(tme2.score("home sales top",indexer.inv))
     print(tme2.score_vectoriel("home sales top",indexer.inv))
+
+def test2():
+    docs = ["the new home has home been saled on top forecasts",
+            "the home sales rise in july",
+            "there is an increase in sales in july",
+            "july encounter a new home sales rise"]
+    parsed = parser.loadCollection(docs)
+    indexer = indexerSimple.IndexerSimple(parsed.docs)
+    w = weighter.c5(indexer)
+    print(w.getWeightsForDoc(0))
+    v = vectoriel.Vectoriel(w,False)
+    print(v.getScores("home"))
 test1()
+test2()
--- vectoriel.py
+++ vectoriel.py
@@ -1,14 +1,13 @@
-import IRModel
+import iRModel
 
-class Vectoriel(IRModel):
+class Vectoriel(iRModel.IRModel):
     def __init__(self,w,n):
         self.weighter = w
         self.normalized = n
     def getScores(self,query):
-        ind = self.weighter.index.ind
-        ind_inv = self.weighter.index.inv
-        if(normalized == False):
-            req = indexerSimple.counter(req)
+        if(self.normalized == False):
+            ind_inv = self.weighter.indexer.inv
+            req = self.weighter.getWeightsForQuery(query)
             cle_index_inv = ind_inv.keys()
             result={}
             for mot in req:
@@ -26,5 +25,6 @@ class Vectoriel(IRModel):
                 if(mot in cle_index_inv):
                     for cle in ind_inv[mot].keys():
                         normeDoc = np.sqrt(np.sum(np.power(list(ind[cle].values()))))
-                        result[cle] = result.get(cle,0) + (ind_inv[mot][cle]*req[mot])/(normeR+normeDoc)
+                        #result[cle] = result.get(cle,0) + (ind_inv[mot][cle]*req[mot])/(normeR+normeDoc)
+                        result[cle] = result.get(cle,0) + (getWeightsForStem(mot)[cle]*req[mot])/(normeR+normeDoc)
             return result
