1c4977d284f6d15c49654998582e011925116649
keyber <ad.spif@gmail.com>
2019-02-16 13:26:55 +0100

    jelinek

--- modelLangue.py
+++ modelLangue.py
@@ -1,40 +1,40 @@
 import indexerSimple
 import iRModel
-import numpy as np
 
 class ModelLangue(iRModel):
-    """jelinek-Mercer
-    Md modèle langue document: tf_t_d / Sum(tf_k_d)
-    MC modèle Collection  : tf_t_c / Sum(tf_k_c)
-    """
+    """jelinek-Mercer"""
     
     def getScores(self, query, requeteLongue=False):
         #suppose indépendances
         req = indexerSimple.counter(query)
         lamb = .2 if requeteLongue else .8
-        p_q_m = []
-        p_q_m = []
-        for d in []:
-            p_q_m[d] = 1
-            for t,tf in req.items():
-                p_q_m[d] *= self.indexer.ind[d][t] ** tf
         
-        p_q_mc = 1
-        for t,tf in req.items():
-            p_q_mc *= self.indexer.ind[d][t] ** tf
+        len_coll = sum(len(d.T) for d in self.indexer.docs)
         
-        return lamb*p(t,mc) + (1-lamb) * p(t,md)
-    pass
+        #facteurs de lissage de chaque terme de la requete (définis par la collection)
+        p_q_mc = {}
+        for t, tf_q in req.items():
+            p_q_mc[t] = sum(self.indexer.ind[d.I][t] for d in self.indexer.docs) / len_coll
+        
+        #scores de chaque doc
+        scores = {}
+        for d in self.indexer.docs:
+            score = 1
+            for t in req.keys():
+                score *= (1-lamb) * self.indexer.ind_n[d][t]  +  lamb * p_q_mc[t]
+            scores[d] = score
+            
+        return scores
 
-class Okapi:
-    pass
-    #cf 21/42
+class Okapi(iRModel):
+    def getScores(self, query):
+        scores = {}
+        for d in self.indexer.docs:
+            score = idf()*x
+            scores[d] = score
+        return scores
+        
+#cf 21/42
 #valeurs par défaut cf cours
 
 #lambda k1 b par cross validation
-#
-
-
-
-
-    
